5512a1d2e73c507e68e7345ecd267b23393410a4
James0126 <james@terra.money>
2021-11-18 18:59:28 +0900

    Add parsing logic

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-const readError = () => {
-  const a = "abc";
+import erorrs from "./errors.json";
 
-  return "";
+const readError = (message: string) => {
+  const data = Object.entries(erorrs).map(([key, value]) => {
+    return { key, value };
+  });
+
+  for (const error of data) {
+    const { key, value } = error;
+    if (message.includes(key)) {
+      // uppercase first letter
+      return value.charAt(0).toUpperCase() + value.slice(1);
+    }
+  }
+  return undefined;
 };
 
 export default readError;
