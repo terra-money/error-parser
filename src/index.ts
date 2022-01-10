e95a2030436b3df9c41696e675394f80bbe91926
James0126 <james@terra.money>
2022-01-10 18:35:58 +0900

    npm publish

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@
 const REGEX =
   /(failed to execute message; message index: \d:|dispatch:|reply:|: execute wasm contract failed|Generic error:)/g;
 
-export const parse = (message: string) => {
-  try {
-    const str = message.replace(REGEX, "").trim();
-    return str;
-  } catch {
-    return "";
+const parse = (message: string) => {
+  if (typeof message !== "string") {
+    throw new TypeError("message type is not a string");
   }
+
+  const str = message.replace(REGEX, "").trim();
+  return str;
 };
+
+export default parse;
