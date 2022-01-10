38257bdbe928cd485d6e1b6094c7e38661f741f9
James0126 <james@terra.money>
2022-01-10 16:52:39 +0900

    Remove error.json

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,10 @@
-import erorrs from "./errors.json";
-
 // Useless keyword
 const REGEX =
   /(failed to execute message; message index: \d:|dispatch:|reply:|: execute wasm contract failed|Generic error:)/g;
 
 const readError = (message: string) => {
-  try {
     const str = message.replace(REGEX, "").trim();
-    for (const [key, value] of Object.entries(erorrs)) {
-      if (str.includes(key)) {
-        return uppercaseFirstLetter(value);
-      }
-    }
-    return uppercaseFirstLetter(str);
-  } catch {
-    return undefined;
-  }
+    return str
 };
 
 export default readError;
-
-const uppercaseFirstLetter = (str: string) =>
-  str.charAt(0).toUpperCase() + str.slice(1);
