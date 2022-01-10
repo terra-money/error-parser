c96b5776a50eace9b0cc73b945353354d2b87de3
James0126 <james@terra.money>
2022-01-10 17:05:36 +0900

    prepare for npm publish

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ const REGEX =
   /(failed to execute message; message index: \d:|dispatch:|reply:|: execute wasm contract failed|Generic error:)/g;
 
 const readError = (message: string) => {
-    const str = message.replace(REGEX, "").trim();
-    return str
+  const str = message.replace(REGEX, "").trim();
+  return str;
 };
 
 export default readError;
