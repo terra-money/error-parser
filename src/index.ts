f5dac1889a8cfa9f571d9dd9a724faf79173e164
James0126 <james@terra.money>
2022-01-10 17:08:52 +0900

    change parse function name

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@
 const REGEX =
   /(failed to execute message; message index: \d:|dispatch:|reply:|: execute wasm contract failed|Generic error:)/g;
 
-const readError = (message: string) => {
+const parse = (message: string) => {
   const str = message.replace(REGEX, "").trim();
   return str;
 };
 
-export default readError;
+export default parse;
